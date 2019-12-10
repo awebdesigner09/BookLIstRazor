60b344f2d204548c56d5446a5f1b8b23c4e35dbe
Naga Sai Katakamsetty <awebdesigner09@gmail.com>
2019-12-10 06:44:50 +0530

    Completed

--- Migrations/20191116013811_addISBNtoBook.Designer.cs
+++ Migrations/20191116013811_addISBNtoBook.Designer.cs
@@ -3,14 +3,16 @@ using BooklistRazor.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BooklistRazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191116013811_addISBNtoBook")]
+    partial class addISBNtoBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ wwwroot/js/bookList.js
@@ -0,0 +1,68 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable(){
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/Book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                       <div class="text-center">
+                    <a href="/BookList/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:ponter;width:70px">
+                    Edit
+                    </a>
+                    &nbsp;
+                    <a onclick="Delete('/api/book?id='+${data})" class="btn btn-danger text-white" style="cursor:ponter;width:70px">
+                    Delete
+                    </a>
+                    </div>`;
+                },
+                "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        dangerMode: true,
+        buttons:true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
